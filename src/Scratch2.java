d3ce689f857552e06f922e1c3f86ca74926d0131
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-28 23:40:00 +0530

    Add description as comment.

--- src/Scratch2.java
+++ src/Scratch2.java
@@ -4,5 +4,7 @@
  *  Time: 11:38 PM
  */
 
+// Same code as in Scratch1.java class but the only difference is in this code we are taking input
+// from the end-user using a message Dialog.
 public class Scratch2 {
 }
