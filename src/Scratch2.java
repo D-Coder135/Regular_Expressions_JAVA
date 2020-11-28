5d2e7726aa64388c2912c2882f499d79d6ad8877
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-28 23:40:12 +0530

    Add main() method

--- src/Scratch2.java
+++ src/Scratch2.java
@@ -7,4 +7,7 @@
 // Same code as in Scratch1.java class but the only difference is in this code we are taking input
 // from the end-user using a message Dialog.
 public class Scratch2 {
+    public static void main(String[] args) {
+
+    }
 }
