c1cdcf5933816840eebdf10cd21eb172eb4acca5
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-29 00:06:50 +0530

    Add some Fixup!

--- src/Scratch2.java
+++ src/Scratch2.java
@@ -8,8 +8,8 @@ import javax.swing.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-// Same code as in Scratch1.java class but the only difference is in this code we are taking input
-// from the end-user using a message Dialog.
+// Same code of Regular Expression as in Scratch1.java class for valid Aadhar Card Number but the only difference
+//  is in this code we are taking input from the end-user using a message dialog.
 public class Scratch2 {
     public static void main(String[] args) {
         // Step 1: Use the Pattern class to define a regular expression.
@@ -22,7 +22,7 @@ public class Scratch2 {
                 JOptionPane.PLAIN_MESSAGE);
         Matcher matcher = pattern.matcher(input);
 
-        // For matching the test String with the Regular Expression, we have a mathces() method which
+        // For matching the test String with the Regular Expression, we have a matches() method which
         // returns boolean value.
         if (matcher.matches()) {
             JOptionPane.showMessageDialog(null,
