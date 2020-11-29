701f2bfe48971c117db9fdd26ed631bdc511916e
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-29 23:14:44 +0530

    Add important description for methods as comment

--- src/Scratch5.java
+++ src/Scratch5.java
@@ -16,5 +16,11 @@ public class Scratch5 {
 
         // Step 2: Match the pattern against a sequence of characters using the Matcher class.
         Matcher matcher = pattern.matcher("1234 4465 1134");
+
+        /*
+        * start() method and end() method
+        * We can find the index at which the current match is obtained by using the start() method and index past one
+        * by using the end() method.
+        * */
     }
 }
