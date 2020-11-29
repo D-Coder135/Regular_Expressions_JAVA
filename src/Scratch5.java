c0d2595702c2f6f6d028de50ae3b23642ed399aa
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-29 23:12:42 +0530

    Pass the regular expression as parameter to the compile() method

--- src/Scratch5.java
+++ src/Scratch5.java
@@ -12,7 +12,7 @@ import java.util.regex.Pattern;
 public class Scratch5 {
     public static void main(String[] args) {
         // Step 1: Use the Pattern class to define a regular expression.
-        Pattern pattern = Pattern.compile("");
+        Pattern pattern = Pattern.compile("\\d{4}");
 
         // Step 2: Match the pattern against a sequence of characters using the Matcher class.
         Matcher matcher = pattern.matcher("");
