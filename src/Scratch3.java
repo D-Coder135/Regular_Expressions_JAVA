82ffcc89efeea3897e2e536e0f814dbaaf774b8e
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-29 20:34:39 +0530

    Pass the test string as the parameter to the matcher() method

--- src/Scratch3.java
+++ src/Scratch3.java
@@ -15,6 +15,6 @@ public class Scratch3 {
         Pattern pattern = Pattern.compile("\\d{4}");
 
         // Step 2: Match the pattern against a sequence of characters using the Matcher class.
-        Matcher matcher = pattern.matcher("");
+        Matcher matcher = pattern.matcher("1234 5456 8952");
     }
 }
