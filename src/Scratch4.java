79653ea9b30c1395b0ae2f0b02df8b262c4fcc0b
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-29 20:52:11 +0530

    Create an object of the Pattern class using the compile() method

--- src/Scratch4.java
+++ src/Scratch4.java
@@ -7,8 +7,11 @@
 // This code is similar to the code in Scratch3.java class.
 // In this code group() method is described and is used along with the find() method.
 
+import java.util.regex.Pattern;
+
 public class Scratch4 {
     public static void main(String[] args) {
         // Step 1: Use the Pattern class to define a regular expression.
+        Pattern pattern = Pattern.compile("");
     }
 }
