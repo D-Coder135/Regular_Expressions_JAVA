d245b77c99feef0ec6cdc844282223618496a8c4
Devansh Shukla <devansh.shukla_cs19@gla.ac.in>
2020-11-29 20:31:16 +0530

    Create an object of Pattern class using compile() method.

--- src/Scratch3.java
+++ src/Scratch3.java
@@ -6,8 +6,11 @@
 
 // Same code of Regular Expression as in Scratch2.java class for valid Aadhar Card Number.
 
+import java.util.regex.Pattern;
+
 public class Scratch3 {
     public static void main(String[] args) {
         // Step 1: Use the Pattern class to define a regular expression.
+        Pattern pattern = Pattern.compile("");
     }
 }
